a31c8627e6c68796d18456ba5173fa061da728f2
Sebastien Gissinger <sgissinger@outlook.com>
2020-08-02 00:18:26 +0200

    add _p and relocalizePath tests

---
+++ test/i18n-_p.spec.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const chai = require('chai')
+const i18n = require('../i18n')
+
+chai.should()
+
+describe('i18n._p', () => {
+    it('should localize a path', () => {
+        i18n.init({
+            localesDirectory: 'test/locales'
+        })
+        i18n.pathPrefix = '/'
+
+        const expected = '/fr-fr/apple'
+        const actual = i18n._p('fr-fr', '/apple')
+
+        actual.should.be.equal(expected)
+    })
+
+    it('should localize a path having a pathPrefix', () => {
+        i18n.init({
+            localesDirectory: 'test/locales'
+        })
+        i18n.pathPrefix = '/blog/'
+
+        const expected = '/fr-fr/apple'
+        const actual = i18n._p('fr-fr', '/apple')
+
+        actual.should.be.equal(expected)
+    })
+
+    it('should localize a path (enhance11tydata)', () => {
+        i18n.init({
+            localesDirectory: 'test/locales'
+        })
+        i18n.pathPrefix = '/'
+
+        const eleventyData = i18n.enhance11tydata({}, 'fr-fr')
+
+        const expected = '/fr-fr/apple'
+        const actual = eleventyData._p('/apple')
+
+        actual.should.be.equal(expected)
+    })
+})
---
+++ test/i18n-relocalizePath.spec.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const chai = require('chai')
+const i18n = require('../i18n')
+
+chai.should()
+
+describe('i18n.relocalizePath', () => {
+    it('should relocalizePath a path', () => {
+        i18n.init({
+            localesDirectory: 'test/locales'
+        })
+        i18n.pathPrefix = '/'
+
+        const expected = '/fr-fr/apple'
+        const actual = i18n.relocalizePath('fr-fr', '/en-us/apple')
+
+        actual.should.be.equal(expected)
+    })
+
+    it('should relocalizePath a path having a pathPrefix', () => {
+        i18n.init({
+            localesDirectory: 'test/locales'
+        })
+        i18n.pathPrefix = '/blog/'
+
+        const expected = '/fr-fr/apple'
+        const actual = i18n.relocalizePath('fr-fr', '/blog/en-us/apple')
+
+        actual.should.be.equal(expected)
+    })
+})
