9e3bb2d04e80dd729e9d3cf9e472bccda7a4a011
Sebastien Gissinger <sgissinger@outlook.com>
2020-08-01 12:59:52 +0200

    use single quote instead of double quote

--- test/dynamic-interpolation.js
+++ test/dynamic-interpolation.js
@@ -9,13 +9,13 @@ describe('Dynamic interpolation', () => {
     })
 
     it('should return a string with tokens replaced by obj properties', () => {
-        const translation = "${value1} ${value2} or not ${value1} ${value2}"
+        const translation = '${value1} ${value2} or not ${value1} ${value2}'
         const obj = {
-            value2: "be",
-            value1: "to"
+            value2: 'be',
+            value1: 'to'
         }
 
-        const expected = "to be or not to be"
+        const expected = 'to be or not to be'
         const actual = dynamic_interpolation(translation, obj)
 
         actual.should.be.equal(expected)
--- test/i18n-getStandardLocale.js
+++ test/i18n-getStandardLocale.js
@@ -15,24 +15,24 @@ describe('i18n.getStandardLocale()', () => {
         })
 
         it('should returns the full locale', () => {
-            const expected = "fr-fr"
-            const actual = i18n.getStandardLocale("fr-fr")
+            const expected = 'fr-fr'
+            const actual = i18n.getStandardLocale('fr-fr')
 
             actual.should.be.equal(expected)
         })
 
         it('should returns the lang locale', () => {
-            const expected = "fr"
-            const actual = i18n.getStandardLocale("fr")
+            const expected = 'fr'
+            const actual = i18n.getStandardLocale('fr')
 
             actual.should.be.equal(expected)
         })
 
         it('should throw an exception if locale does not match', () => {
             (() => {
-                i18n.getStandardLocale("benl")
+                i18n.getStandardLocale('benl')
             })
-            .should.throw("Locale benl does not match regex /^(?<lang>.{2})(?:-(?<country>.{2}))*$/")
+            .should.throw('Locale benl does not match regex /^(?<lang>.{2})(?:-(?<country>.{2}))*$/')
         })
     })
 
@@ -45,24 +45,24 @@ describe('i18n.getStandardLocale()', () => {
         })
 
         it('should returns the full locale', () => {
-            const expected = "nl-be"
-            const actual = i18n.getStandardLocale("benl")
+            const expected = 'nl-be'
+            const actual = i18n.getStandardLocale('benl')
 
             actual.should.be.equal(expected)
         })
 
         it('should returns the lang locale', () => {
-            const expected = "nl"
-            const actual = i18n.getStandardLocale("nl")
+            const expected = 'nl'
+            const actual = i18n.getStandardLocale('nl')
 
             actual.should.be.equal(expected)
         })
 
         it('should throw an exception if locale does not match', () => {
             (() => {
-                i18n.getStandardLocale("nl-be")
+                i18n.getStandardLocale('nl-be')
             })
-            .should.throw("Locale nl-be does not match regex /^(?:(?<country>.{2}))*(?<lang>.{2})$/")
+            .should.throw('Locale nl-be does not match regex /^(?:(?<country>.{2}))*(?<lang>.{2})$/')
         })
     })
 })
