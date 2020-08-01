c527b4111cccb2aa2de49980b9edee7cab1e1f5f
Sebastien Gissinger <sgissinger@outlook.com>
2020-08-01 18:55:42 +0200

    rename test files

--- test/i18n-normalizePath.spec.js
+++ test/i18n-normalizePath.spec.js
@@ -3,36 +3,33 @@
 const chai = require('chai')
 const sinon = require('sinon')
 const i18n = require('../i18n')
-const eleventyConfig = require('@11ty/eleventy/src/Config')
 
 chai.should()
 
 describe('i18n.normalizePath', () => {
     it('should not normalize path when pathPrefix is /', () => {
-        sinon.stub(eleventyConfig, 'getConfig')
-            .returns({ pathPrefix: '/' })
+        i18n.pathPrefix = '/'
 
         const expected = '/tatayoyo'
         const actual = i18n.normalizePath('/tatayoyo')
 
         actual.should.be.equal(expected)
-
-        sinon.restore()
     })
 
     it('should normalize path when pathPrefix is /blog/', () => {
-        sinon.stub(eleventyConfig, 'getConfig')
-            .returns({ pathPrefix: '/blog/' })
+        i18n.pathPrefix = '/blog/'
 
         const expected = 'post-12345'
         const actual = i18n.normalizePath('/blog/post-12345')
 
         actual.should.be.equal(expected)
-
-        sinon.restore()
     })
 
-    it('should call @11ty/eleventy/src/Config.getConfig() only once', () => {
+    it('should call @11ty/eleventy/src/Config.getConfig only once', () => {
+        i18n.pathPrefix = undefined
+
+        const eleventyConfig = require('@11ty/eleventy/src/Config')
+
         const stub = sinon.stub(eleventyConfig, 'getConfig')
             .returns({ pathPrefix: '/blog/' })
 
