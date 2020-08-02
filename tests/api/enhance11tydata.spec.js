090630e8fbd63830aea07b2da2a859597b3eb4eb
Sebastien Gissinger <sgissinger@outlook.com>
2020-08-02 17:21:55 +0200

    add test linux style and windows style path in enhance11tydata

--- tests/api/enhance11tydata.spec.js
+++ tests/api/enhance11tydata.spec.js
@@ -41,19 +41,40 @@ describe('enhance11tydata', () => {
         actual.locale.should.be.equal(expected.locale)
     })
 
-    it('should use path basename to retrieve the locale', () => {
+    it('should use path basename to retrieve the locale (linux)', () => {
         i18n.init()
 
         sinon.stub(fs, 'existsSync').returns(true)
 
         const expected = {
             lang: 'fr',
+            langDir: 'ltr',
             locale: 'fr-fr'
         }
+        const actual = i18n.enhance11tydata({}, '/dummy/eleventy-plugin-i18n-gettext-demo/src/fr-fr')
 
+        actual.lang.should.be.equal(expected.lang)
+        actual.langDir.should.be.equal(expected.langDir)
+        actual.locale.should.be.equal(expected.locale)
+
+        sinon.restore()
+    })
+
+
+    it('should use path basename to retrieve the locale (windows)', () => {
+        i18n.init()
+
+        sinon.stub(fs, 'existsSync').returns(true)
+
+        const expected = {
+            lang: 'fr',
+            langDir: 'ltr',
+            locale: 'fr-fr'
+        }
         const actual = i18n.enhance11tydata({}, 'D:\\dummy\\eleventy-plugin-i18n-gettext-demo\\src\\fr-fr')
 
         actual.lang.should.be.equal(expected.lang)
+        actual.langDir.should.be.equal(expected.langDir)
         actual.locale.should.be.equal(expected.locale)
 
         sinon.restore()
